b6312e3737329223657622d4a17d539f4ae9a5b9
godot63 <lcalmbach@gmail.com>
2021-10-18 07:25:29 +0200

    cn.DB_PSSW must be DB_PASS

--- const.py
+++ const.py
@@ -26,7 +26,7 @@ if socket.gethostname().lower() in DEV_MACHINES:
     DB_PORT = "5432"
 else:
     DB_USER = "dxkqwxlfbaffnk"
-    DB_PSSWD = os.environ.get('DB_PSSWD')
+    DB_PASS = os.environ.get('DB_PSSWD')
     DB_HOST = 'ec2-54-216-17-9.eu-west-1.compute.amazonaws.com'
     DB_DATABASE = 'd3f49ft1g3uc8t'
     DB_PORT = "5432"
